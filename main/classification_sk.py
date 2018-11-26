829962c2ed4283dac7e54621ce88f789037991a7
EysiW <eystein@waade.net>
2018-11-26 22:33:08 +0800

    data works, classification need mod

--- main/classification_sk.py
+++ main/classification_sk.py
@@ -6,35 +6,37 @@ from sklearn.svm import SVC, NuSVC, LinearSVC
 from sklearn.metrics import confusion_matrix
 from main.data import make_Dictionary
 from main.data import extract_features
+from data import nof_directory
 import pandas as pd
 from data import goto
 
 # Create a dictionary of words with its frequency
 os.chdir('..')
-train_dir = 'dataset\\bare\\part2'
+train_dir = 'dataset/set1/bare/part1'
 dictionary = make_Dictionary(train_dir)
 
 # Prepare feature vectors per training mail and its labels
+nof_ham = nof_directory('dataset/set2/enron2/ham')
+nof_spam = nof_directory('dataset/set2/enron2/spam')
+train_labels = np.zeros(nof_ham+nof_spam)
+train_labels[:nof_ham] = 1
 
-train_labels = np.zeros(702)
-train_labels[351:701] = 1
+train_matrix = pd.read_csv('feature_enron2')
 
-train_matrix = pd.read_csv('feature_matrix')
-print(type(train_matrix))
 
 # Training SVM and Naive bayes classifier
-if False:
-    model1 = MultinomialNB()
-    model2 = LinearSVC()
-    model1.fit(train_matrix,train_labels)
-    model2.fit(train_matrix,train_labels)
 
-    # Test the unseen mails for Spam
-    test_dir = 'test-mails'
-    test_matrix = extract_features(test_dir)
-    test_labels = np.zeros(260)
-    test_labels[130:260] = 1
-    result1 = model1.predict(test_matrix)
-    result2 = model2.predict(test_matrix)
-    print(confusion_matrix(test_labels,result1))
-    print(confusion_matrix(test_labels,result2))
+model1 = MultinomialNB()
+model2 = LinearSVC()
+model1.fit(train_matrix,train_labels)
+model2.fit(train_matrix,train_labels)
+
+# Test the unseen mails for Spam
+test_dir = 'test-mails'
+test_matrix = extract_features(test_dir)
+test_labels = np.zeros(260)
+test_labels[130:260] = 1
+result1 = model1.predict(test_matrix)
+result2 = model2.predict(test_matrix)
+print(confusion_matrix(test_labels,result1))
+print(confusion_matrix(test_labels,result2))
---
+++ main/data.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib as mat
+import os
+from collections import Counter
+import time
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
+    all_words = []
+    for mail in emails:
+        with open(mail) as m:
+            for i, line in enumerate(m):
+                if i == 2:  # Body of email is only 3rd line of text file
+                    words = line.split()
+                    all_words += words
+
+    dictionary = Counter(all_words)
+    removed_chars = list()
+
+    for key in dictionary.keys():
+        if key.isalpha() == False:
+            removed_chars.append(key)
+        elif len(key) == 1:
+            removed_chars.append(key)
+    for key in removed_chars:
+        dictionary.pop(key)
+    dictionary = dictionary.most_common(3000)
+    return dictionary
+
+
+def extract_features(root_dir):
+    emails = search_maildirectory(root_dir)
+    nof_email = emails[0].__len__() + emails[1].__len__()
+    docID = 0
+    dictionary = make_Dictionary('dataset/set1/bare/part1')
+    features_matrix = np.zeros((nof_email,3001))
+    train_labels = np.zeros(nof_email)
+    for folder in emails:
+        for mail in folder:
+            with open(mail) as m:
+                print(m)
+                all_words = []
+                for line in m:
+                    words = line.split()
+                    all_words += words
+                for word in all_words:
+                  wordID = 0
+                  for i,d in enumerate(dictionary):
+                    if d[0] == word:
+                      wordID = i
+                      features_matrix[docID,wordID] = all_words.count(word)
+            train_labels[docID] = int(mail.split(".")[-2] == 'spam')
+            features_matrix[docID,-1] = int(mail.split(".")[-2] == 'spam')
+            docID = docID + 1
+    return features_matrix, train_labels
+
+
+def nof_directory(directory):
+    files = [os.path.join(directory, f) for f in os.listdir(directory)]
+    return files.__len__()
+
+
+def goto(level):
+    print(os.getcwd())
+    for i in range(level):
+        os.chdir('..')
+    print(os.getcwd())
+
+def search_maildirectory(mail_dir):
+    """
+    searches directory with folders including emails
+    :param mail_dir:
+    :return: list with name of emails
+    :rtype: list()
+    """
+    dir = [os.path.join(mail_dir,f) for f in os.listdir(mail_dir)]
+    mail_list = list()
+    for folders in dir:
+        files = [os.path.join(folders, f) for f in os.listdir(folders)]
+        mail_list.append(files)
+
+    return mail_list
+
+#os.chdir('..')
+#print(nof_directory('dataset/set1/bare/part1'))
+#extract_features('dataset/set2/enron1')
+#feat = extract_features('dataset/set2/enron2')
+#pd.DataFrame(feat[0]).to_csv('feature_enron2')
+#pd.DataFrame(feat[1]).to_csv('test_label_enron2')
+#print(search_maildirectory('dataset\\set2\\enron1')[1].__len__())
